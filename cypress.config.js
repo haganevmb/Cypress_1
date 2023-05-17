94772db54773808320489fa5cf6bda8974dd85af
Victor <1asir.mora.victor@gmail.com>
2023-05-17 14:15:25 +0200

    actualización configuración cypress

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: 'c8j78q',
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
